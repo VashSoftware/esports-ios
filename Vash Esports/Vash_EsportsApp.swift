e43968e22ae37de21ee6b61f87bce816f59ba15d
Stan Runge <stan@stanrunge.dev>
2024-02-07 05:12:42 +0100

    Initial Commit

---
+++ Vash Esports/Vash_EsportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vash_EsportsApp.swift
+//  Vash Esports
+//
+//  Created by Stan Runge on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Vash_EsportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
